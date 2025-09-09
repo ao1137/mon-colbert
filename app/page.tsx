f6c975590c0cc816d6cf74dc9b49e5473c3ab08a
Komnena <bq20026@shibaura-it.ac.jp>
2025-09-10 02:11:57 +0900

    feat: Display initial welcome page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">
+        Mon Colbert(モン・コルベール) へようこそ！
+      </h1>
+      <p className="mt-4">
+        最初のページ編集に成功しました。
+      </p>
+    </main>
+  );
+}
